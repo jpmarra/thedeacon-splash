6c7b95e888e76ffe1a2522a83b9d33da5455aea3
jmarra <jp.marra@mlb.com>
2019-01-07 17:59:07 -0500

    fix image paths

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,3 +1,7 @@
+import Celebrate from './assets/images/Deacon-Intro-01_Celebrate.jpg';
+import Relax from './assets/images/Deacon-Intro-02_Relax.jpg';
+import Work from './assets/images/Deacon-Intro-03_Work.jpg'
+
 const dictionary = {
     splashIntro: 'The Deacon is a boutique hotel and event space offering a unique, thoughtfully-curated guest experience, designed for a diverse set of occasions and gatherings.',
     learnMore: 'Learn More',
@@ -7,21 +11,21 @@ const dictionary = {
         {
             title: 'CELEBRATE',
             copy: 'An intimate wedding, a family gathering, a holiday party in a breathtaking space. Vaulted ceilings and arched windows set the backdrop of this space’s rich history, while modern touches and design nods inspired by the art and humor of John Baldessari, make The Deacon a contemporary curated space.',
-            image: '/assets/images/Deacon-Intro-01_Celebrate.jpg',
+            image: Celebrate,
             imageDescriptor: 'Celebrate',
             orientation: 'left'
         },
         {
             title: 'RELAX & ENJOY',
             copy: 'Eight uniquely designed rooms, a master chef’s kitchen and entertainment space are just a few of the amenities provided at The Deacon. We have partnered with local businesses to bring you a unique Philadelphia experience from your cup of morning coffee to unwinding after sight-seeing at some of our favorite places.',
-            image: '/assets/images/Deacon-Intro-02_Relax.jpg',
+            image: Relax,
             imageDescriptor: 'Relax',
             orientation: 'right'
         },
         {
             title: 'WORK & COLLABORATE',
             copy: 'Sometimes a more creative environment makes for a more productive and inspired group. We strongly believe this and are making The Deacon available to those who believe the same.',
-            image: '/assets/images/Deacon-Intro-03_Work.jpg',
+            image: Work,
             imageDescriptor: 'Work',
             orientation: 'left'
         }
