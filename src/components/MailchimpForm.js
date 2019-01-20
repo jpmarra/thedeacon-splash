260af596a5880d481586f2edbb1d40bfe3a73151
jmarra <jp.marra@mlb.com>
2019-01-20 18:22:09 -0500

    resets form on submit

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { validate } from 'email-validator';
 
-const MailchimpForm = ({ onValidated }) => {
-    let email;
-    const submitForm = () => {
-        if (validate(email.value)) {
-            console.log('THAT WORKS!')
-            onValidated({ EMAIL: email.value })
+class MailchimpForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: ''
+        }
+
+        this.submitForm = this.submitForm.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    submitForm(e) {
+        e.preventDefault();
+
+        if (validate(this.state.email)) {
+            this.props.onValidated({ EMAIL: this.state.email });
+            this.setState({ email: ''});
         }
     }
 
-    return (
-        <div action='' className='deacon__contact-newsletter-form'>
-            <input
-                className='deacon__contact-newsletter-form-input'
-                type='email'
-                ref={node => (email = node)}
-                placeholder='Email Address'
-            />
-            <button className='deacon__contact-newsletter-form-button' onClick={submitForm}>
-                SIGN UP
-            </button>
-        </div>
-    );
+    handleChange(e) {
+        e.preventDefault();
+        let email = e.target.value;
+        this.setState({ email });
+    }
+
+    render() {
+        return (
+            <form className='deacon__contact-newsletter-form' onSubmit={this.submitForm}>
+                <input
+                    className='deacon__contact-newsletter-form-input'
+                    type='email'
+                    value={this.state.email}
+                    placeholder='Email Address'
+                    onChange={this.handleChange}
+                />
+                <button className='deacon__contact-newsletter-form-button' type='submit'>
+                    SIGN UP
+                </button>
+            </form>
+        );
+    }
 };
 
 export default MailchimpForm;
