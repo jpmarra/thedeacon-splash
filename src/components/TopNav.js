57aec3a8d55c0c12d8e6c21b048ca53fb08f4d56
JP Marra <jp.marra@me.com>
2022-02-03 23:03:19 -0500

    bumps react and removes exp link

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import Dictionary from '../dictionary';
+import React from "react";
+import Dictionary from "../dictionary";
 
 const TopNav = ({ handleScroll }) => (
-    <div className='deacon__top-nav'>
-        <button className='deacon__top-nav-option' onClick={() => handleScroll('#about')}>ABOUT</button>
-        <button className='deacon__top-nav-option' onClick={() => handleScroll('#map')}>NEIGHBORHOOD</button>
-        <div className='deacon__top-nav-option-link-container'>
-            <a target="_blank" rel="noopener noreferrer" className='deacon__top-nav-option' href='https://experiences.thedeaconphl.com'>EXPERIENCES</a>
-        </div>
-        <div className='deacon__top-nav-option-link-container'>
-                <a target="_blank" rel="noopener noreferrer" className='deacon__top-nav-option' href='https://docs.google.com/forms/d/e/1FAIpQLSewpxRgFBUVAKqkErC_6EQ8ffaYc77SDzf9K7SEceXA5CeZ-w/viewform?vc=0&c=0&w=1'>EVENTS</a>
-        </div>
-        <button className='deacon__top-nav-option' onClick={() => handleScroll('#contact')}>CONTACT</button>
-        <a
-            className='deacon__top-nav-book-now'
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://app.thebookingbutton.com/properties/thedeacondirect'
+    <div className="deacon__top-nav">
+        <button
+            className="deacon__top-nav-option"
+            onClick={() => handleScroll("#about")}
         >
-            <span className="button-text">BOOK<br/>NOW</span>
+            ABOUT
+        </button>
+        <button
+            className="deacon__top-nav-option"
+            onClick={() => handleScroll("#map")}
+        >
+            NEIGHBORHOOD
+        </button>
+        <div className="deacon__top-nav-option-link-container">
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="deacon__top-nav-option"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSewpxRgFBUVAKqkErC_6EQ8ffaYc77SDzf9K7SEceXA5CeZ-w/viewform?vc=0&c=0&w=1"
+            >
+                EVENTS
+            </a>
+        </div>
+        <button
+            className="deacon__top-nav-option"
+            onClick={() => handleScroll("#contact")}
+        >
+            CONTACT
+        </button>
+        <a
+            className="deacon__top-nav-book-now"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://app.thebookingbutton.com/properties/thedeacondirect"
+        >
+            <span className="button-text">
+                BOOK
+                <br />
+                NOW
+            </span>
         </a>
     </div>
 );
