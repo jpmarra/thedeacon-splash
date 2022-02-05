902feca270570d26fda7edc05a09f0b306fbdc38
JP Marra <jp.marra@me.com>
2022-02-05 02:39:48 -0500

    add form embed

--- src/Events.js
+++ src/Events.js
@@ -3,24 +3,40 @@ import { Link } from "react-router-dom";
 import EventForm from "./components/EventForm";
 import { ReactComponent as Logo } from "./assets/icons/Logo-Hor-Large.svg";
 import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 
 export const Events = () => {
+    React.useEffect(() => {
+        const formRoot = document.getElementById("form-root");
+        const script = document.createElement("script");
+        script.src =
+            "https://api.tripleseat.com/v1/leads/ts_script.js?lead_form_id=22323&public_key=f5ddd67014eacb646e88e06bb8f586ebc5986a0d&inline_form=true";
+        formRoot.appendChild(script);
+    }, []);
     return (
-        <div className="events-page">
-            <Link to="/" className="events-page__link">
-                <Logo className="events-page__logo" />
-            </Link>
-            <div className="events-page__container">
-                <div className="events-page__form">
-                    <h1 className="events-page__form--header">Event Inquiry</h1>
-                    <div className="events-page__form--desc">
-                        To request an event, please fill out the following form.
+        <>
+            <Helmet>
+                <script src="https://www.google.com/recaptcha/api.js"></script>
+            </Helmet>
+            <div className="events-page">
+                <Link to="/" className="events-page__link">
+                    <Logo className="events-page__logo" />
+                </Link>
+                <div className="events-page__container">
+                    <div className="events-page__form">
+                        <h1 className="events-page__form--header">
+                            Event Inquiry
+                        </h1>
+                        <div className="events-page__form--desc">
+                            To request an event, please fill out the following
+                            form.
+                        </div>
+                        <div id="form-root" />
                     </div>
-                    <EventForm />
                 </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </>
     );
 };
 
